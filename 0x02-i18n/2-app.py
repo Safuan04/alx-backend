07a9d05addaa7b7802bd334cd384cc459ed314e8
Safuan04 <elmai.safuan@gmail.com>
2024-02-06 20:55:20 +0100

    2. Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Get locale from request"""
+""" Get locale from request
+"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
@@ -7,9 +8,8 @@ from flask_babel import Babel
 app = Flask(__name__)
 
 
-class Config():
-    """ class that has a LANGUAGES class attribute
-    -   equal to ["en", "fr"]
+class Config:
+    """ Configuration class
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -17,19 +17,22 @@ class Config():
 
 
 def get_locale():
-    """ determine the best match with our supported languages
+    """ Determine the best match with our supported languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+babel = Babel(app, locale_selector=get_locale)
+app.config.from_object(Config)
+
+
 @app.route("/")
 def welcome():
-    """ simply outputs “Welcome to Holberton” as page title (<title>)
-    -   and “Hello world” as header (<h1>)"""
+    """ Simply outputs “Welcome to Holberton” as page title (<title>)
+    -   and “Hello world” as header (<h1>)
+    """
     return render_template('2-index.html')
 
 
-babel = Babel(app, locale_selector=get_locale)
-app.config.from_object(Config)
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
