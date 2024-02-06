31eb2d41c4db8b167683d2c5244d9fbd87a596d0
Safuan04 <elmai.safuan@gmail.com>
2024-02-06 21:37:42 +0100

    3. Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" Get locale from request
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Configuration class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Determine the best match with our supported languages
+    """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route("/")
+def welcome():
+    """ Simply outputs “Welcome to Holberton” as page title (<title>)
+    -   and “Hello world” as header (<h1>)
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
